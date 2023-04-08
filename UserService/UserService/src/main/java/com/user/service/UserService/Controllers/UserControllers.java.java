86c607bfa3a69bad410648b12b198256c78fd9fc
Raja Rohan Ray <43091846+rohan9521@users.noreply.github.com>
2023-04-09 04:06:13 +0530

    Created HotelService

--- UserService/UserService/src/main/java/com/user/service/UserService/Controllers/UserControllers.java.java
+++ UserService/UserService/src/main/java/com/user/service/UserService/Controllers/UserControllers.java.java
@@ -4,6 +4,7 @@ public class UserController{
 	@AutoWired
 	private UserService userService;
 	//create
+	@PostMapping
 	public ResponseEntity<User> createUser(@RequestBody User user){
 		User userNew = userService.saveUser(user);
 		return ResponseEntity.status(HttpStatus.CREATEDy).body(userNew);
@@ -12,6 +13,11 @@ public class UserController{
 	
 	//get single user
 @GetMapping("/{userId}")
+public ResponseEntity<User> getSingleUser(@PathVariable String UserId){
+	User retireivedUser = userService.getUser(userId);
+	return ResponseEntity.ok(retireivedUser);
+}
+@GetMapping
 public ResponseEntity<User> getSingleUser(@PathVariable String Userid){
 	List<User> allUser = userService.getAllUser();
 	return ResponseEntity.ok(allUser);
