72199c5240e5555968b74e37779c7411913ea9a8
rohan9521 <rayrohan909@gmail.com>
2023-05-13 03:18:13 +0530

    Added api-gateway

--- UserService/src/main/java/com/user/service/UserService/UserServiceApplication.java
+++ UserService/src/main/java/com/user/service/UserService/UserServiceApplication.java
@@ -1,17 +1,15 @@
 package com.user.service.UserService;
 
-import com.user.service.UserService.Entities.User;
-import com.user.service.UserService.Repository.UserRepository;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableFeignClients
-//@EnableEurekaClient
-@EntityScan(basePackageClasses = User.class)
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
